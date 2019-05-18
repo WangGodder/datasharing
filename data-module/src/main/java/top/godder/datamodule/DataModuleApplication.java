c15b6c0d2a2cbd874eb807926fa99665aa90c892
WangGodder <32242917+WangGodder@users.noreply.github.com>
2019-05-18 13:10:45 +0800

    add jwt in data module pom

---
+++ data-module/src/main/java/top/godder/datamodule/DataModuleApplication.java
@@ -0,0 +1,13 @@
+package top.godder.datamodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataModuleApplication.class, args);
+    }
+
+}
