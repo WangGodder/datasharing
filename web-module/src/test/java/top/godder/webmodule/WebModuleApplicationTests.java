e35fa97966eb98116ee3bcff4f3d7a0b8354b94c
WangGodder <32242917+WangGodder@users.noreply.github.com>
2019-05-23 22:07:51 +0800

    add web module

---
+++ web-module/src/test/java/top/godder/webmodule/WebModuleApplicationTests.java
@@ -0,0 +1,16 @@
+package top.godder.webmodule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebModuleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
