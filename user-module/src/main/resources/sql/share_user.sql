094fdcab6ef6a81970be2f7c13047fd81b5537b0
WangGodder <32242917+WangGodder@users.noreply.github.com>
2019-06-12 02:02:18 +0800

    fix auto login bug

---
+++ user-module/src/main/resources/sql/share_user.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql
+Source Server Version : 80011
+Source Host           : localhost:3306
+Source Database       : 
+
+Target Server Type    : MYSQL
+Target Server Version : 80011
+File Encoding         : 65001
+
+Date: 2019-06-11 16:19:18
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
